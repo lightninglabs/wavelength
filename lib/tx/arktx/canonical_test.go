a01c31c63fb29cc9105e8223da6282bab41ec812
Andras Banki-Horvath <bhandras@gmail.com>
2025-12-20 13:19:34 +0100

    tx: split arktx and checkpoint primitives
    Move Ark tx canonicalization and checkpoint-building helpers out of
    `lib/tx/oor` into dedicated `lib/tx/arktx` and `lib/tx/checkpoint`
    packages.
    
    Keep compatibility wrappers in `lib/tx/oor` so existing call sites and
    current tests continue to compile while the stack migrates.

---
+++ lib/tx/arktx/canonical_test.go
@@ -0,0 +1,51 @@
+package arktx
+
+import (
+	"testing"
+
+	"github.com/btcsuite/btcd/wire"
+	"github.com/lightninglabs/darepo-client/lib/scripts"
+	"github.com/stretchr/testify/require"
+)
+
+// TestCanonicalizeOrderingSortsAndValidates asserts CanonicalizeOrdering
+// produces a transaction that passes ValidateCanonicalTx, even if the input tx
+// is not canonical.
+func TestCanonicalizeOrderingSortsAndValidates(t *testing.T) {
+	t.Parallel()
+
+	tx := wire.NewMsgTx(TxVersion)
+	tx.AddTxIn(&wire.TxIn{
+		PreviousOutPoint: wire.OutPoint{
+			Hash:  [32]byte{2},
+			Index: 1,
+		},
+	})
+	tx.AddTxIn(&wire.TxIn{
+		PreviousOutPoint: wire.OutPoint{
+			Hash:  [32]byte{1},
+			Index: 0,
+		},
+	})
+
+	// Add outputs in non-canonical order and with anchor not last.
+	tx.TxOut = append(tx.TxOut,
+		&wire.TxOut{
+			Value:    1,
+			PkScript: []byte{0x52},
+		},
+		scripts.AnchorOutput(),
+		&wire.TxOut{
+			Value:    2,
+			PkScript: []byte{0x51},
+		},
+	)
+
+	err := CanonicalizeOrdering(tx)
+	require.NoError(t, err)
+
+	err = ValidateCanonicalTx(tx)
+	require.NoError(t, err)
+
+	require.True(t, IsAnchorOutput(tx.TxOut[len(tx.TxOut)-1]))
+}
