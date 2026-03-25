f083235e2c8fe20c774b6dcead453444ca9a99af
Olaoluwa Osuntokun <laolu32@gmail.com>
2026-03-24 17:52:05 -0700

    db: regenerate sqlc for client fee tables
    Run make sqlc to regenerate type-safe Go code from the new client fee
    accounting queries. Adds LedgerEntry, Account, and LedgerEventType
    model types along with query methods to the Querier interface.

--- db/actordelivery/sqlc/models.go
+++ db/actordelivery/sqlc/models.go
@@ -8,6 +8,16 @@ import (
 	"database/sql"
 )
 
+type Account struct {
+	AccountID   string
+	AccountName string
+	AccountType string
+}
+
+type AccountType struct {
+	AccountType string
+}
+
 type AskResult struct {
 	PromiseID  string
 	ResultBlob []byte
@@ -33,6 +43,21 @@ type FsmCheckpoint struct {
 	StateData []byte
 	Version   int32
 	UpdatedAt int64
+}
+
+type LedgerEntry struct {
+	EntryID       int64
+	DebitAccount  string
+	CreditAccount string
+	AmountSat     int64
+	RoundID       []byte
+	EventType     string
+	Description   string
+	CreatedAt     int64
+}
+
+type LedgerEventType struct {
+	EventType string
 }
 
 type MailboxMessage struct {
